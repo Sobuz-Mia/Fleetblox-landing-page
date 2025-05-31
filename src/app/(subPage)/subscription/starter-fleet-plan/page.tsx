136ac373f80a2c215d296657e9c5f12fa8898a3c
MD Sajjad Hossen <sajjadmugdho@yahoo.com>
2025-06-01 02:28:09 +0600

    [Done] added starter-fleet-plan  all

---
+++ src/app/(subPage)/subscription/starter-fleet-plan/page.tsx
@@ -0,0 +1,107 @@
+import RightArrowIcon from "@/components/icons/RightArrowIcon";
+import Link from "next/link";
+import RealtimeFleetAwareness from "../eagle-eye-fleet/components/RealtimeFleetAwareness";
+import ReducedRiskAndDowntime from "../eagle-eye-fleet/components/ReducedRiskAndDowntime";
+import AuditReadySection from "../eagle-eye-fleet/components/AuditReadySection";
+import ExpansionCrossSites from "../eagle-eye-fleet/components/ExpansionCrossSites";
+
+
+const page = () => {
+
+    return (
+        <>
+            {/* hero section */}
+            <section className="bg-[#FAFAFF] pt-[100px] pb-[30px] overflow-hidden relative">
+                <div className="max-w-[1200px] mx-auto px-5 flex flex-col items-center py-[120px] text-center relative z-50 ">
+                    <h1 className="text-[28px] text-[#0336BC] font-bold font-montserrat">
+                        Eagle Eye Fleet
+                    </h1>
+                    <h2 className="text-[52px] font-bold text-[#04082C] leading-normal md:leading-[1.1] max-w-[800px] w-full mx-auto mb-[10px] mt-[5px] font-montserrat">
+                        Turn Fleet Blind Spots into Strategic Insights
+                    </h2>
+                    <p className="text-[16px] text-[#333] leading-6 font-openSans mb-[30px]">
+                        {` Eagle Eye is not just another fleet utility—it's your operational
+          edge. Designed for today's fleet operation, it keeps you in
+          compliance, notifies you of danger before it occurs, and captures full
+          clarity on all vehicles and all locations—without hardware,
+          installations, or man hours`}
+                    </p>
+                    {/* hover:w-[168.72px] w-[190.72px] */}
+                    <Link aria-label="Get started with FleetBlox" href="/getting-started">
+                        <button className="hidden transition-all bg-[#2D65F2] hover:bg-[#0336BC] font-openSans text-white-primary text-white duration-300 hover:w-[190.72px] w-[168.72px] lg:flex items-center px-4 py-3 text-base font-bold rounded-md group">
+                            <div className="z-50 whitespace-nowrap">View Pricing Plan</div>
+                            <div className="z-50 transform transition-transform opacity-0 group-hover:opacity-100 -translate-x-4 duration-300 group-hover:translate-x-0">
+                                <RightArrowIcon />
+                            </div>
+                        </button>
+                    </Link>
+                    {/* <Link aria-label="Get started with FleetBlox" href="/getting-started">
+          <button className="lg:hidden bg-[#2D65F2] hover:bg-[#0336BC] text-white w-full flex px-4 py-3 text-[14px] font-openSans font-bold rounded-md justify-center">
+            Start today
+          </button>
+        </Link> */}
+                </div>
+                {/* Blurred spotlight ellipse */}
+                <div
+                    className={`absolute left-1/2 top-2/3 max-w-[664px] w-full max-h-[664px] h-full -translate-x-1/2 -translate-y-1/2 rounded-[664px] bg-[#FBEECA] opacity-40 blur-[200px]`}
+                ></div>
+            </section>
+            {/* Realtime fleet awareness section */}
+            <RealtimeFleetAwareness />
+            {/* Reduced Risk and Downtime section */}
+            <ReducedRiskAndDowntime />
+            {/* Always audit ready section */}
+            <AuditReadySection />
+            {/* Expansions cross sites */}
+            <ExpansionCrossSites />
+
+            {/* Call to action section */}
+
+            <section className="py-[80px]">
+                <div className="max-w-[1000px] mx-auto w-full text-center px-5">
+                    <h2 className="text-[#04082C] w-full mx-auto font-montserrat text-[36px] lg:text-[36px] font-bold text-center leading-[24px] mb-[10px]">
+                        Clarity Is Your Competitive Edge
+                    </h2>
+                    <p className="text-[#7D7D7D] text-[16px] leading-6 font-openSans">
+                        As you can’t manage what you can’t see, Eagle Eye gives you full-spectrum visibility into your fleet without hardware, hassle or guesswork. From real-time condition insights to automated compliance tracking, it delivers the control, foresight  and operational intelligence that modern fleet operators need to stay ahead.
+
+                    </p>
+                    <Link
+                        aria-label="Eagle eye your fleet"
+                        href="#"
+                        className="mt-[20px] lg:mt-[30px] flex justify-center"
+                    >
+                        <button className="hidden transition-all bg-[#2D65F2] hover:bg-[#0336BC] font-openSans text-white-primary text-white duration-300 hover:w-[200px] w-[190px] lg:flex items-center px-4 py-3 text-base font-bold rounded-md group">
+                            <div className="z-20 whitespace-nowrap">Eagle eye your fleet</div>
+                            <div className="z-10 transform transition-transform opacity-0 group-hover:opacity-100 -translate-x-4 duration-300 group-hover:translate-x-0">
+                                <RightArrowIcon />
+                            </div>
+                        </button>
+                    </Link>
+                </div>
+
+                <div className="max-w-[1200px] mx-auto w-full px-5 mt-[60px]">
+                    <section className="my-8 md:my-10 border border-[#DFDFDF] h-[250px] xs:h-[280px] sm:h-[350px] md:h-[425px] rounded-[24px] bg-white overflow-hidden">
+                        <div className="relative w-full h-full">
+                            <video
+                                autoPlay
+                                loop
+                                muted
+                                playsInline
+                                preload="auto"
+                                className="absolute inset-0 w-full h-full object-cover"
+                            >
+                                <source src="/videos/egale-eye-fleet.mp4" type="video/mp4" />
+                            </video>
+                            svg
+
+                        </div>
+                    </section>
+                </div>
+
+            </section>
+        </>
+    );
+};
+
+export default page;
