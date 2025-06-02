5e848784914176bbc962ecb91758934988f3474b
Sobuz-Mia <sobuz.info0298@gmail.com>
2025-06-02 20:55:34 +0600

    updated the header as per humayun bhai feedback

--- src/components/modules/home/FleetSolution.tsx
+++ src/components/modules/home/FleetSolution.tsx
@@ -6,42 +6,80 @@ import fleetSolutionImg from "../../../assets/fleetSolution.png";
 import fleetSolutionImg2 from "../../../assets/fleetSolution.png";
 import Image from "next/image";
 import NavigationIcon from "@/components/icons/NavigationIcon";
+type StatItem = {
+  icon: React.FC;
+  value: number;
+  suffix?: string;
+  description: string;
+};
+type Feature = {
+  title: string;
+  description: string;
+};
 const StatsSection = () => {
+  const statsData: StatItem[] = [
+    {
+      icon: DecisionMakingIcon,
+      value: 90,
+      description: "Faster Deployment Across Locations",
+    },
+    {
+      icon: DriverAccountabilityIcon,
+      value: 3,
+      suffix: "K",
+      description: "Faster ROI on Fleet Operations",
+    },
+    {
+      icon: CostOperatingIcon,
+      value: 60,
+      description: "Increase in Asset Utilization",
+    },
+  ];
+  const fleetFeatures: Feature[] = [
+    {
+      title: "Lower Costs",
+      description: "Eliminate expensive hardware installation and maintenance.",
+    },
+    {
+      title: "Streamlined Operations",
+      description: "Manage everything from a cloud-based, all-in-one platform.",
+    },
+    {
+      title: "Scalability",
+      description:
+        "Add or remove vehicles effortlessly, free from hardware limitations.",
+    },
+    {
+      title: "Enhanced Security",
+      description: "Advanced encryption ensures your data stays protected.",
+    },
+    {
+      title: "Increased Efficiency",
+      description:
+        "Leverage AI tools to optimize scheduling and reduce downtime.",
+    },
+  ];
   return (
     <section className="py-[60px] flex flex-col items-center justify-center mx-auto max-w-[1200px] px-5">
       {/* counter component */}
       <div className="flex flex-col lg:flex-row justify-center gap-20 py-[30px] mb-[60px]">
-        <div className="text-center flex flex-col items-center">
-          <DecisionMakingIcon />
-          <h1 className="text-[#04082C] text-[36px] md:text-[52px] font-bold">
-            <AnimatedCounter end={65} />
-          </h1>
-          <p className="text-[#333] font-openSans text-[16px] font-semibold leading-6">
-            Faster Decision-Making and Setup
-          </p>
-        </div>
-        <div className="text-center flex flex-col items-center">
-          <CostOperatingIcon />
-          <h1 className="text-[#04082C] text-[36px] md:text-[52px] font-bold">
-            <AnimatedCounter end={25} />
-          </h1>
-          <p className="text-[#333] text-[16px] font-openSans font-semibold leading-6">
-            Reduction in Operating Costs
-          </p>
-        </div>
-        <div className="text-center flex flex-col items-center">
-          <DriverAccountabilityIcon />
-          <h1 className="text-[#04082C] text-[36px] md:text-[52px] font-bold ">
-            <AnimatedCounter end={30} />
-          </h1>
-          <p className="text-[#333] text-[16px] font-openSans font-semibold leading-6">
-            Boost in Driver Accountability
-          </p>
-        </div>
+        {statsData.map((stat, index) => (
+          <div key={index} className="text-center flex flex-col items-center">
+            <stat.icon />
+            <h1 className="text-[#04082C] text-[36px] md:text-[52px] font-bold">
+              <AnimatedCounter end={stat.value} isString={stat.suffix} />
+            </h1>
+            <p className="text-[#333] font-openSans text-[16px] font-semibold leading-6">
+              {stat.description}
+            </p>
+          </div>
+        ))}
       </div>
-
       {/* Laptop View */}
-      <div className="hidden md:flex lg:flex border mx-auto max-w-[1200px] border-[#DFDFDF] rounded-[24px] flex-col lg:flex-row">
+      <div
+        style={{ boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.14)" }}
+        className=" hidden md:flex lg:flex mx-auto max-w-[1200px]  rounded-[24px] flex-col lg:flex-row"
+      >
         <div className="flex flex-col items-center lg:items-start w-full lg:w-1/2 p-10 mx-auto">
           <h1 className="text-[#04082C] text-[36px] text-center lg:text-left font-bold mb-5 font-montserrat leading-[1.1]">
             Experience a Smarter Way to Manage Your Fleet
@@ -58,54 +96,15 @@ const StatsSection = () => {
             </button>
           </Link> */}
           <div className="pt-10">
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px]  leading-5 ">
-                <h2 className="font-bold text-[#333]">Lower Costs</h2>
-                <p className="text-[#7D7D7D]">
-                  Eliminate expensive hardware installation and maintenance.
-                </p>
+            {fleetFeatures.map((feature, index) => (
+              <div key={index} className="flex gap-[16px] mb-[30px]">
+                <NavigationIcon />
+                <div className=" text-[14px]  leading-5 ">
+                  <h2 className="font-bold text-[#333]">{feature?.title}</h2>
+                  <p className="text-[#7D7D7D]">{feature?.description}</p>
+                </div>
               </div>
-            </div>
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px]  leading-5 ">
-                <h2 className="font-bold text-[#333]">
-                  Streamlined Operations
-                </h2>
-                <p className="text-[#7D7D7D]">
-                  Manage everything from a cloud-based, all-in-one platform.
-                </p>
-              </div>
-            </div>
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px]  leading-5 ">
-                <h2 className="font-bold text-[#333]">Scalability</h2>
-                <p className="text-[#7D7D7D]">
-                  Add or remove vehicles effortlessly, free from hardware
-                  limitations.
-                </p>
-              </div>
-            </div>
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px]  leading-5 ">
-                <h2 className="font-bold text-[#333]">Enhanced Security</h2>
-                <p className="text-[#7D7D7D]">
-                  Advanced encryption ensures your data stays protected.
-                </p>
-              </div>
-            </div>
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px]  leading-5 ">
-                <h2 className="font-bold text-[#333]">Increased Efficiency</h2>
-                <p className="text-[#7D7D7D]">
-                  Leverage AI tools to optimize scheduling and reduce downtime.
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
         <div className="lg:w-1/2 w-full mx-auto lg:mt-10 xl:mt-20 flex justify-center items-center relative">
@@ -138,62 +137,20 @@ const StatsSection = () => {
             </div>
           </div>
           <div className="pt-10 ">
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px] leading-5 ">
-                <h2 className="font-bold text-[#333] font-openSans">
-                  Lower Costs
-                </h2>
-                <p className="text-[#7D7D7D] font-openSans">
-                  Eliminate expensive hardware installation and maintenance.
-                </p>
+            {fleetFeatures.map((feature, index) => (
+              <div key={index} className="flex gap-[16px] mb-[30px]">
+                <NavigationIcon />
+                <div className=" text-[14px] leading-5 ">
+                  <h2 className="font-bold text-[#333] font-openSans">
+                    {feature?.title}
+                  </h2>
+                  <p className="text-[#7D7D7D] font-openSans">
+                    {feature?.description}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px]  leading-5 ">
-                <h2 className="font-bold text-[#333] font-openSans">
-                  Streamlined Operations
-                </h2>
-                <p className="text-[#7D7D7D] font-openSans">
-                  Manage everything from a cloud-based, all-in-one platform.
-                </p>
-              </div>
-            </div>
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px]  leading-5 ">
-                <h2 className="font-bold text-[#333] font-openSans">
-                  Scalability
-                </h2>
-                <p className="text-[#7D7D7D]  font-openSans">
-                  Add or remove vehicles effortlessly, free from hardware
-                  limitations.
-                </p>
-              </div>
-            </div>
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px]  leading-5 ">
-                <h2 className="font-bold text-[#333] font-openSans">
-                  Enhanced Security
-                </h2>
-                <p className="text-[#7D7D7D] font-openSans">
-                  Advanced encryption ensures your data stays protected.
-                </p>
-              </div>
-            </div>
-            <div className="flex gap-[16px] mb-[30px]">
-              <NavigationIcon />
-              <div className=" text-[14px]  leading-5 ">
-                <h2 className="font-bold text-[#333] font-openSans">
-                  Increased Efficiency
-                </h2>
-                <p className="text-[#7D7D7D] font-openSans">
-                  Leverage AI tools to optimize scheduling and reduce downtime.
-                </p>
-              </div>
-            </div>
+            ))}
+
             {/* <Link
               aria-label="Get started with FleetBlox"
               href="/getting-started"
