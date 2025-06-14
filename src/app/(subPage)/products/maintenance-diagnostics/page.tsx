b937c30eefa800674c62808d13715afdb3aeece6
Sobuz-Mia <sobuz.info0298@gmail.com>
2025-06-14 20:43:02 +0600

    recheck the all page under paltform and resolve some ui bugs

--- src/app/(subPage)/products/maintenance-diagnostics/page.tsx
+++ src/app/(subPage)/products/maintenance-diagnostics/page.tsx
@@ -124,7 +124,7 @@ const page = () => {
         </div>
       </section>
       {/* how to connect and connect step started */}
-      <section className="bg-[#FAFAFF] py-[100px]">
+      <section className="bg-[#FAFAFF] py-[100px] px-5">
         <h3 className="text-[#04082C] text-[36px] font-bold leading-[1.1] text-center ">
           Simplify Maintenance in 3 Easy Steps
         </h3>
@@ -135,7 +135,7 @@ const page = () => {
         <div className="max-w-[1200px] w-full mx-auto mt-[60px] mb-10 px-5">
           <FleetConnectionStep />
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 relative max-w-[1200px] mx-auto w-full gap-5 justify-items-center items-center px-5">
+        <div className="grid grid-cols-1 lg:grid-cols-3 relative max-w-[1200px] mx-auto w-full gap-5 justify-items-center items-center">
           <div className="absolute hidden lg:block left-1/2 top-1/2 mt-[90px] max-w-[664px] max-h-[664px] h-full w-full -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#FBEECA] opacity-40 blur-[200px]"></div>
           <StepCard
             title="Assign a Technician"
