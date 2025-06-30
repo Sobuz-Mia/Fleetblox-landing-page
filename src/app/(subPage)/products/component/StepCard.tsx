d15edad4321e32e7845ae18b8bb3245b456931ae
Sobuz-Mia <sobuz.info0298@gmail.com>
2025-06-30 09:28:27 +0600

    optimized code of the step card issue

--- src/app/(subPage)/products/component/StepCard.tsx
+++ src/app/(subPage)/products/component/StepCard.tsx
@@ -5,19 +5,25 @@ interface StepCardProps {
   description: string;
   imagePath: string;
   height?: string;
+  stepper?: number;
 }
 
 const StepCard = ({
   title,
   description,
   imagePath,
-  height = "h-[540px]",
+  stepper = 1,
+  height = "h-[600px] lg:h-[540px]",
 }: StepCardProps) => {
+  console.log("Received Stepper:", stepper);
   return (
     <div
       className={`relative z-[200] top-[0px] mx-auto transform flex-col ${height} w-full`}
     >
       <div className="bg-white absolute z-50 rounded-[24px] py-10 px-[30px] flex flex-col gap-[10px] w-full h-full">
+        {/* <div className="max-w-[44px] max-h-[44px] w-full mx-auto mb-2 h-full border-2 border-[#0336BC] rounded-[60px] p-1 lg:hidden flex items-center justify-center text-[#0336BC] font-openSans text-[22px] font-bold">
+          {stepper}
+        </div> */}
         <div className="font-openSans text-center lg:text-left">
           <h3 className="text-[#04082C] text-[22px] font-bold">{title}</h3>
           <p className="text-[14px] text-[#7D7D7D] leading-5 mt-[5px]">
