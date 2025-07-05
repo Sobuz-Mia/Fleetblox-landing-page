974ced254a227e4af974d742bb2055e49f531e6f
sajjad-mugdho <sajjadmugdho@yahoo.com>
2025-07-05 20:29:30 +0600

    [Done] added all details

--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { TermsAndServiceDynamic } from '../lazy'
-
+import React from "react";
+import { TermsAndServiceDynamic } from "../lazy";
+import Navbar from "@/components/ui/shared/Navbar";
+import Footer from "@/components/ui/shared/Footer";
 
 function page() {
-    return (
-        <TermsAndServiceDynamic />
-
-    )
+  return (
+    <>
+      <Navbar />
+      <TermsAndServiceDynamic />
+      <Footer />
+    </>
+  );
 }
 
-export default page
+export default page;
