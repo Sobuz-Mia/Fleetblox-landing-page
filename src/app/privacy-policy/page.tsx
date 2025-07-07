f0ecf992f98cb9098b5842ae40a553b8932b6cd2
sajjad-mugdho <sajjadmugdho@yahoo.com>
2025-07-07 22:37:51 +0600

    [Added] privecy policy page design

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,9 +1,15 @@
+import Navbar from "@/components/ui/shared/Navbar"
 import { PrivecyPolicyDynamic } from "../lazy"
+import Footer from "@/components/ui/shared/Footer"
 
 
 function page() {
     return (
+        <>
+        <Navbar/>
         <PrivecyPolicyDynamic />
+        <Footer/>
+        </>
     )
 }
 
