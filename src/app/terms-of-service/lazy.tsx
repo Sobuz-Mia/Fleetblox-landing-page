a1907113bb42db5f3c7d07519642333ed58ca42f
sajjad-mugdho <sajjadmugdho@yahoo.com>
2025-07-09 18:39:04 +0600

    [Done] added end user licence

--- src/app/terms-of-service/lazy.tsx
+++ src/app/terms-of-service/lazy.tsx
@@ -4,3 +4,17 @@ import dynamic from "next/dynamic";
 export const TermsOfServiceLazy = dynamic(() => import("./Terms"), {
   ssr: false,
 });
+
+export const DataProcessingLazy = dynamic(
+  () => import("./data-processing/DataProcessing"),
+  {
+    ssr: false,
+  }
+);
+
+export const EndUserLicenseLazy = dynamic(
+  () => import("./end-user-license/EndUserLicense"),
+  {
+    ssr: false,
+  }
+);
