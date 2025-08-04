4ded987414908f59e8c25490a36b58cfb1e787eb
Sobuz-Mia <sobuz.info0298@gmail.com>
2025-08-04 22:35:50 +0600

    updated env

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -72,7 +72,7 @@ export default function Home() {
       localStorage.removeItem("isGetDemo");
     }
   }, []);
-
+  console.log(process.env.NEXT_PUBLIC_API_URL);
   return (
     <div>
       <Navbar />
